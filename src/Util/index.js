ca2d89a7a325900b2896e2eac88a7144172515f7
sma2lbao <sma2lbao@qq.com>
2017-06-29 10:56:27 +0800

    static page

--- src/Util/index.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import util from './util.js'
-export default {
-  install: function (Vue, ) {
-    Object.defineProperty(Vue.prototype, '$util', {value: util});
-  }
-}
--- src/store/types.js
+++
@@ -1,3 +0,0 @@
-export const SIGNIN = 'SIGNIN'
-export const SIGNOUT = 'SIGNOUT'
-export const TOGGLEMENU = 'TOGGLEMENU'
